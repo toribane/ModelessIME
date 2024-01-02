712c062e5e1b0fe230742d266e3a783bf516cae7
kachaya <katsuhiko.toba@outlook.jp>
2024-01-03 07:23:17 +0900

    辞書とコピーライトを更新

--- app/src/main/java/io/github/kachaya/ime/SoftKey.java
+++ app/src/main/java/io/github/kachaya/ime/SoftKey.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023  kachaya
+ * Copyright 2023-2024  kachaya
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
